52628e78d946be1be3e327a1adc2b8a3ef6d4c00
your-mirror <miklinsib@yandex.ru>
2016-11-08 23:28:42 +0300

    add task 2

--- src/index.js
+++ src/index.js
@@ -6,7 +6,26 @@ const app = express();
 app.get('/', function (req, res) {
   const a = _.get(req.query, 'a', 0);
   const b = _.get(req.query, 'b', 0);
-  res.send({ result: Number(a) + Number(b) });
+  res.send(200, Number(a) + Number(b));
+});
+
+app.get('/name', function (req, res) {
+  const match = _.get(req.query, 'fullname', '').match(/([\S]+)\s?([\S]*)\s?([\S]*)\s?(.*)/i);
+  let shortName = '';
+  if (match !== null && match[4].length === 0) {
+    const parts = _.chain(match).drop().compact().value();
+    shortName += _.capitalize(_.last(parts).toLowerCase());
+    if (parts.length > 1) {
+      shortName += _.chain(parts).dropRight().map(function(string) {
+        const symbol = _.capitalize(string.charAt(0));
+        return  ` ${symbol}.`;
+      }).join('');
+    }
+  } else {
+    shortName = 'Invalid fullname';
+  }
+
+  res.send(200, shortName);
 });
 
 app.listen(5000, function () {
