d2e4564761fd954122b2fe33a0bb816b2eca4ea9
your-mirror <miklinsib@yandex.ru>
2016-11-08 21:01:47 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-function hello(name) {
-  console.log(`Hello ${name}`); // eslint-disable-line
-}
-hello('JS World');
+import express from 'express';
+import _ from 'lodash';
+
+const app = express();
+
+app.get('/', function (req, res) {
+  const a = _.get(req.query, 'a', 0);
+  const b = _.get(req.query, 'b', 0);
+  res.send({ result: Number(a) + Number(b) });
+});
+
+app.listen(5000, function () {
+  console.log('Example app listening on port 3000!');
+});
