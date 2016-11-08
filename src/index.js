0d626d16434ee5c9e40346fdbe812779c93f62b5
your-mirror <miklinsib@yandex.ru>
2016-11-09 00:40:13 +0300

    fixes for task 2

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import _ from 'lodash';
+import cors from 'cors';
 
 const app = express();
-
+app.use(cors());
 app.get('/', function (req, res) {
   const a = _.get(req.query, 'a', 0);
   const b = _.get(req.query, 'b', 0);
@@ -10,19 +11,23 @@ app.get('/', function (req, res) {
 });
 
 app.get('/name', function (req, res) {
-  const match = _.get(req.query, 'fullname', '').match(/([\S]+)\s?([\S]*)\s?([\S]*)\s?(.*)/i);
+  console.log(_.get(req.query, 'fullname', ''));
+  const fullName = _.get(req.query, 'fullname', '');
+  const wrongSymbols = fullName.match(/[\d_\/]/);
+  const match = _.get(req.query, 'fullname', '').match(/([\S]+)\s*([\S]*)\s*([\S]*)\s*(.*)/i);
+  const parts = _.chain(match).drop().compact().value();
+
+  if (fullName.length === 0 || wrongSymbols !== null || parts.length > 3) {
+    res.send(200, 'Invalid fullname');
+    return;
+  }
   let shortName = '';
-  if (match !== null && match[4].length === 0) {
-    const parts = _.chain(match).drop().compact().value();
-    shortName += _.capitalize(_.last(parts).toLowerCase());
-    if (parts.length > 1) {
-      shortName += _.chain(parts).dropRight().map(function(string) {
-        const symbol = _.capitalize(string.charAt(0));
-        return  ` ${symbol}.`;
-      }).join('');
-    }
-  } else {
-    shortName = 'Invalid fullname';
+  shortName += _.capitalize(_.last(parts).toLowerCase());
+  if (parts.length > 1) {
+    shortName += _.chain(parts).dropRight().map(function(string) {
+      const symbol = _.capitalize(string.charAt(0));
+      return  ` ${symbol}.`;
+    }).join('');
   }
 
   res.send(200, shortName);
