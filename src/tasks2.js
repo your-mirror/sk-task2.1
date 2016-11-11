2041c218ce984bddcb6d561a8a7fffebf55d434b
your-mirror <miklinsib@yandex.ru>
2016-11-11 22:05:58 +0300

    task3 done

---
+++ src/tasks2.js
@@ -0,0 +1,3 @@
+/**
+ * Created by your-mirror on 11.11.16.
+ */
---
+++ src/tasks3.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import _ from 'lodash';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.get('/task2', function (req, res) {
+  const a = _.get(req.query, 'a', 0);
+  const b = _.get(req.query, 'b', 0);
+  res.send(200, Number(a) + Number(b));
+});
+
+app.get('/task2b', function (req, res) {
+  const fullName = _.get(req.query, 'fullname', '');
+  const wrongSymbols = fullName.match(/[\d_\/]/);
+  const match = _.get(req.query, 'fullname', '').match(/([\S]+)\s*([\S]*)\s*([\S]*)\s*(.*)/i);
+  const parts = _.chain(match).drop().compact().value();
+
+  if (fullName.length === 0 || wrongSymbols !== null || parts.length > 3) {
+    res.send(200, 'Invalid fullname');
+  }
+  let shortName = '';
+  shortName += _.capitalize(_.last(parts).toLowerCase());
+  if (parts.length > 1) {
+    shortName += _.chain(parts).dropRight().map(function(string) {
+      const symbol = _.capitalize(string.charAt(0));
+      return  ` ${symbol}.`;
+    }).join('');
+  }
+
+  res.send(200, shortName);
+});
+
+app.get('/task2c', function (req, res) {
+  const usernameUrl = _.get(req.query, 'username', '');
+  const match = usernameUrl
+    .match(/(?:https:|http:)?(?:\/\/)?(?:[a-z0-9\.-]+\.[a-z0-9-]+)?\/?@?([a-z\.\_]+)/i);
+  const username = _.chain(match).drop().value();
+  if (username.length === 0) {
+    res.send(200, 'Invalid fullname');
+  }
+  res.send(200, '@' + username);
+});
+
+app.listen(5000, function () {
+  console.log('Example app listening on port 5000!');
+});
