d7a0891b636bf2f3d4e9b8a10704c15fc0b77225
your-mirror <miklinsib@yandex.ru>
2016-11-13 22:22:28 +0300

    task 3a done

--- src/tasks3.js
+++ src/tasks3.js
@@ -1,12 +1,57 @@
 import express from 'express';
 import _ from 'lodash';
+import fetch from 'node-fetch';
 
+const pathName = 'tasks3a';
+const pcUrl = 'https://gist.githubusercontent.com/isuvorov/ce6b8d87983611482aac89f6d7bc0037/raw/pc.json';
 const router = express.Router();
+let pc = {};
+fetch(pcUrl)
+  .then(async (res) => {
+    pc = await res.json();
+  })
+  .catch(err => {
+    console.log('Чтото пошло не так:', err);
+  });
 
-router.get('/task3a', function (req, res) {
-  const a = _.get(req.query, 'a', 0);
-  const b = _.get(req.query, 'b', 0);
-  res.send(200, Number(a) + Number(b));
+router.get(`/${pathName}*`, function (req, res) {
+  let data;
+  // on base url
+  if (_.includes([`/${pathName}`, `/${pathName}/`], req.path)) {
+    data = pc;
+  }
+  else if (req.path === '/tasks3a/volumes') {
+    data = volumes();
+  } else {
+    data = base(req);
+  }
+
+  if (data === 'Not Found') {
+    res.send(404, data);
+  } else {
+    res.send(200, JSON.stringify(data));
+  }
 });
+
+function base(request) {
+  'use strict';
+  if (request.path.match(/[^\w\/]/) !== null) {
+    return 'Not Found';
+  }
+  const property = _.chain(request.path).split('/').drop(2).join('.').value();
+  return (_.get(pc, property) !== undefined) ? _.get(pc, property) : 'Not Found';
+}
+
+function volumes() {
+  'use strict';
+  let mergedHdd = _.reduce(pc.hdd, function(result, hdd, key) {
+    result[hdd.volume] = (result.hasOwnProperty(hdd.volume) === false)
+      ? hdd.size : result[hdd.volume] + hdd.size;
+    return result;
+  }, {});
+  mergedHdd = _.mapValues(mergedHdd, function(size) { return size + 'B'; });
+
+  return mergedHdd;
+}
 
 export { router }
